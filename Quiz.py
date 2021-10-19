6c5a1f3a7fdd9000294f2f58e017ac97911bb454
Jordan Hart <p22jhart@fairfieldprep.org>
2021-10-19 07:49:42 -0700

    quiz

---
+++ Quiz.py
@@ -0,0 +1,59 @@
+
+h=("DAY OF THE WEEK")
+0=("saturday")
+1= ("sunday")
+2=("monday")
+3=("tuesday")
+4=("wednesday")
+5=("thursday")
+6=("friday")
+
+m=("Month")
+3=("march")
+4=("April")
+5=("May")
+6=("june")
+7=("july")
+8=("august")
+9=("september")
+10=("october")
+11=("november")
+12=("december")
+13=("january")
+14=("february")
+
+if :
+    0 ("saturday")
+elif: 
+    1 ("sunday")
+elif:
+    2 ("monday")
+elif:
+    3("tuesday")
+elif:
+    4("wednesday")
+elif:
+    5("thursday")
+elif:
+    6("friday")
+
+if: 
+    3 ("march")
+elif:
+    4 ("april")
+elif
+
+
+q=("day of the month")
+
+j=("is the century")
+
+k=("is he year")
+   
+
+  
+input("Enter the year of the date:") 
+input("Enter the month of the date:")
+input("Enter the day of the date:")
+
+h=(int(q)+ int(26**(m+1)//int(10)+int(k)+int(k)//int(4)+int(j)//4+int(5**j))%7)
